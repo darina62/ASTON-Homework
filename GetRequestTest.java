193483bbf4bd560aa33cc710badaea2f9a63d95a
darina62 <kira.graf.14@mail.ru>
2026-02-20 12:42:22 +0300

    Create GetRequestTest.java

---
+++ GetRequestTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GetRequestTest {
+    
+    @Test
+    public void testGetRequest() {
+        RestAssured.baseURI = "https://postman-echo.com";
+        
+        given()
+            .param("foo1", "bar1")
+            .param("foo2", "bar2")
+        .when()
+            .get("/get")
+        .then()
+            .statusCode(200)
+            .body("args.foo1", equalTo("bar1"))
+            .body("args.foo2", equalTo("bar2"));
+    }
+    
+    @Test
+    public void testGetRequestFullResponse() {
+        RestAssured.baseURI = "https://postman-echo.com";
+        
+        Response response = given()
+            .param("foo1", "bar1")
+            .param("foo2", "bar2")
+        .when()
+            .get("/get")
+        .then()
+            .extract()
+            .response();
+        
+        // Проверка статус кода (как в pm.test("response is ok"))
+        assertEquals(200, response.statusCode());
+        
+        // Проверка тела ответа (как в pm.test с jsonBody)
+        assertEquals("bar1", response.jsonPath().getString("args.foo1"));
+        assertEquals("bar2", response.jsonPath().getString("args.foo2"));
+        
+        // Дополнительные проверки
+        assertEquals("https://postman-echo.com/get?foo1=bar1&foo2=bar2", 
+                     response.jsonPath().getString("url"));
+        assertNotNull(response.jsonPath().getString("headers.host"));
+    }
+}
