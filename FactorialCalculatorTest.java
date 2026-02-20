e99165dd0d468d00133c48a5131f4c681da7ca1b
darina62 <kira.graf.14@mail.ru>
2026-02-20 09:14:07 +0300

    Create FactorialCalculatorTest.java

---
+++ FactorialCalculatorTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import static org.testng.Assert.*;
+
+public class FactorialCalculatorTest {
+    
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(FactorialCalculator.calculateFactorial(0), 1);
+    }
+    
+    @Test
+    public void testFactorialOfOne() {
+        assertEquals(FactorialCalculator.calculateFactorial(1), 1);
+    }
+    
+    @Test
+    public void testFactorialOfFive() {
+        assertEquals(FactorialCalculator.calculateFactorial(5), 120);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        FactorialCalculator.calculateFactorial(-5);
+    }
+}
