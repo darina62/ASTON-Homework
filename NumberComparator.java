bce7fdce12d9056b54a8df3de9060b1776013136
darina62 <kira.graf.14@mail.ru>
2026-02-20 09:08:56 +0300

    Create NumberComparator.java

---
+++ NumberComparator.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class NumberComparator {
+    
+    public static String compare(int a, int b) {
+        if (a > b) {
+            return a + " больше " + b;
+        } else if (a < b) {
+            return a + " меньше " + b;
+        } else {
+            return a + " равно " + b;
+        }
+    }
+    
+    public static int getMax(int a, int b) {
+        return Math.max(a, b);
+    }
+    
+    public static int getMin(int a, int b) {
+        return Math.min(a, b);
+    }
+}
